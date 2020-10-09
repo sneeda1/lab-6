ff4c0eecc1c3a5521766d2d7e11ffab0d8842a56
sneeda1 <36556631+sneeda1@users.noreply.github.com>
2020-10-09 19:43:43 -0400

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,45 @@
+package com.sw409.blackboard1.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "Courses")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	String title;
+	String authorName;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public Course(Integer id, String title, String authorName) {
+		
+		this.id = id;
+		this.title = title;
+		this.authorName = authorName;
+	}
+	public Course() {
+		
+	}
+	
+	
+
+}
